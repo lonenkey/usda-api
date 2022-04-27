7c346c96a8ee065c6ffa04d4a0faea3e0469a028
Lon Enkey <lonenkey@gmail.com>
2022-04-26 20:09:33 -0500

    added choosing the food.  Still working on trying to limit reply to certain fields

--- usda1.py
+++ usda1.py
@@ -4,14 +4,19 @@ from requests.structures import CaseInsensitiveDict
 
 url = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=ci382bPqyQBylV8bkbeqK7ZQGNeW93lIZFevpXOf"
 
+food = input("What food are you looking for:  ")
+
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 
-data = '{"query": "loquat", "dataType": ["Branded"], "sortBy": "fdcId", "sortOrder": "desc"}'
-
-resp = requests.post(url, headers=headers, data=data)
+data = {"query": "goji" , "dataType": ["Branded"], "maxItems": 1, "format": "abridged"}
+print (data)
+#json.dumps(data)
+print (data)
+resp = requests.post(url, headers=headers, json=data)
 food_data = json.dumps(resp.json(), indent = 4)
-
+food_fields = json.loads(food_data)
 #jsonify = json.dumps(food_data, indent = 4)
-print(food_data)
+#print(food_data)
 #print (jsonify)
+print (food_fields["foods"]["description"])
