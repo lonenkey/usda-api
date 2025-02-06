c14362fb19e9fe88d6c6d2a9ac4de6a69ffe9ced
Lon <lonenkey@gmail.com>
2025-02-06 17:14:46 -0600

    borked

--- usda1.py
+++ usda1.py
@@ -4,17 +4,21 @@ from requests.structures import CaseInsensitiveDict
 import credentials
 
 def main():
-    url = credentials.url
-
     food = input("What food are you looking for:  ")
+    get_items(food)
 
+def get_items(food):
+    url = credentials.url
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
 
     data = {"query": food, "dataType": ["Branded"], "maxItems": 1, "format": "abridged"}
     resp = requests.post(url, headers=headers, json=data)
     food_data = json.dumps(resp.json(), indent = 4)
+    with open("food_data.txt", "w") as f:
+        f.write(food_data)
     food_fields = json.loads(food_data)
+    #print(food_fields)
     hits = (food_fields["totalHits"])
     if hits > 50:
         hits = 50
@@ -22,8 +26,25 @@ def main():
     print ("Matching foods:")
     for itemcount in range(1, hits):
         #print (item, itemcount)
-        print (food_fields["foods"][itemcount]["description"])
+        print (str(itemcount) + " " + food_fields["foods"][itemcount]["description"])
         #itemcount = itemcount+1
+    
+    item_choice = int(input ("Which item would you like to see: "))
+    print (str(item_choice) + " " + food_fields["foods"][item_choice]["description"])
+    print(type(food_fields))
+
+        # Print food name
+    print (f"Nutrients for:", food_fields["foods"][item_choice]["description"])
+    
+    selected_food = food_fields["foods"][item_choice - 1]  
+        # Print food name
+    print(f"Nutrients for: {selected_food['description']}")
+
+    # Print nutrients
+    for nutrient in selected_food["foodNutrients"]:
+        print(f"{nutrient['nutrientName']}: {nutrient['value']} {nutrient['unitName']}")    
+
+ 
 
 if __name__ == "__main__":
     main()
