c3f7268bab2231e4c98460f9af0a98a3354c0175
Lon <lonenkey@gmail.com>
2025-02-07 08:18:48 -0600

    this is a test2

--- usda1.py
+++ usda1.py
@@ -5,9 +5,9 @@ import credentials
 
 def main():
     food = input("What food are you looking for:  ")
-    get_items(food)
+    get_food_items(food)
 
-def get_items(food):
+def get_food_items(food):
     url = credentials.url
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
@@ -18,6 +18,8 @@ def get_items(food):
     with open("food_data.txt", "w") as f:
         f.write(food_data)
     food_fields = json.loads(food_data)
+    with open("food_fields.txt", "w") as f:
+        f.write(str(food_fields))
     #print(food_fields)
     hits = (food_fields["totalHits"])
     if hits > 50:
@@ -28,10 +30,10 @@ def get_items(food):
         #print (item, itemcount)
         print (str(itemcount) + " " + food_fields["foods"][itemcount]["description"])
         #itemcount = itemcount+1
-    
+        
+    # create a try and except block
     item_choice = int(input ("Which item would you like to see: "))
     print (str(item_choice) + " " + food_fields["foods"][item_choice]["description"])
-    print(type(food_fields))
 
         # Print food name
     print (f"Nutrients for:", food_fields["foods"][item_choice]["description"])
@@ -42,9 +44,9 @@ def get_items(food):
 
     # Print nutrients
     for nutrient in selected_food["foodNutrients"]:
-        print(f"{nutrient['nutrientName']}: {nutrient['value']} {nutrient['unitName']}")    
+        print(f"{nutrient['nutrientName']}: {nutrient['value']} {nutrient['unitName']}")   
+        #test 
 
  
-
 if __name__ == "__main__":
     main()
